53ffbc116024a211b97363c587e3b368e2752966
hg <18624087500@163.com>
2019-10-21 22:41:14 +0800

    change content

--- readme.md
+++ readme.md
@@ -6,3 +6,47 @@
 - webpack 源码分析
 
 ## 基本打包配置# webpack4-template
+>webpack
+~~~
+webpack开箱即用只支持js和json两种文件,通过loaders去支持其他文件类型并把它们转化成有效的模块
+~~~
+> entry
+~~~
+webpack程序打包的主入口
+~~~
+> output
+~~~
+打包文件输出目的地
+~~~
+>mode
+~~~
+代表当前打包的模式 - 默认production模式
+~~~
++ production
++ development
++ none
+>module
+~~~
+各种loader加载的区域
+
+loader的加载顺序-链式调用从后往前依次执行
+~~~
+|名称|描述|
+|----|----|
+|babel-loader|转换es6,es7.js语法新特性|
+|css-loader|支持css文件的加载和解析|
+|less-loader|将less转换成css|
+|file-loader|进行图片文字的打包|
+|raw-loader|将文字以字符串的形式导入|
+|thread-loader|用于多进程打包js,css|
+|style-loader|将样式通过style标签插入到head中|
+>plugins
+~~~
+plugins进行实例化的区域
+~~~
+|名称|描述|
+|----|----|
+|optimize-css-assets-webpack-plugin|css压缩|
+|html-webpack-plugin|html压缩|
+|mini-css-extract-plugin|css压缩|
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,81 @@
+"use strict";
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-    entry: "",
-    output: "",
+    entry: {
+        index: "./src/singlePagePack/index.js"
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: '[name]_[chunkhash:4].js'
+    },
     mode: "production",
     module: {
-
+        rules: [
+            {
+                test: /.js$/,
+                use: "babel-loader"
+            },
+            {
+                test: /.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+            {
+                test: /.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "less-loader"
+                ]
+            },
+            {
+                test: /.(png|jepg|jpg|gif)$/,
+                use: [{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name]_[hash:8].[ext]"
+                    }
+                }]
+            },
+            {
+                test: /.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name]_[hash:8].[ext]"
+                        }
+                    }
+                ]
+            }
+        ]
     },
-    plugins: []
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name]_[contenthash:8].css"
+        }),
+        new OptimizeCSSAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano')
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src/singlePagePack/index.html'),
+            filename: 'index.html',
+            chunks: ['index'],
+            inject: true,
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: false
+            }
+        }),
+    ]
 };
