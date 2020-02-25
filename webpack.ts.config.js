873fd87dc8c7eee04d43ac64269304903df72a0f
huguang <18624087500@163.com>
2020-02-25 10:23:57 +0800

    change some config file

--- webpack.ts.config.js
+++ webpack.ts.config.js
@@ -1,19 +1,19 @@
-'use strict';
+
 const path = require('path');
 const glob = require('glob');
 const Webpack = require('webpack');
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin'); //将dll文件insert into html与htmlwebpackplugin搭配使用
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin'); // 将dll文件insert into html与htmlwebpackplugin搭配使用
 const Frienderrorsonly = require('friendly-errors-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');//开启模块的缓存
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');// 开启模块的缓存
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // const speedMeasureWebpackPlugin = require("speed-measure-webpack-plugin"); //webpack package 速度分析
 // const smp = new speedMeasureWebpackPlugin();
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;//构建体积分析插件
-const TerserPlugin = require('terser-webpack-plugin'); //并行压缩
-//实现多页面打包func
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');// 构建体积分析插件
+const TerserPlugin = require('terser-webpack-plugin'); // 并行压缩
+// 实现多页面打包func
 const setMPA = () => {
     const entry = {};
     const htmlwebpackplugins = [];
@@ -28,43 +28,43 @@ const setMPA = () => {
                 new HtmlWebpackPlugin({
                     template: path.join(__dirname, `src/${pagename}/index.html`),
                     filename: `${pagename}.html`,
-                    chunks: [pagename, 'commons'],//script 标签进行引用模块名称
+                    chunks: [pagename, 'commons'], // script 标签进行引用模块名称
                     inject: true,
                     minify: {
                         html5: true,
-                        removeComments: false, //ssr removeComments:false or true
+                        removeComments: false, // ssr removeComments:false or true
                         preserveLineBreaks: false,
                         minifyCSS: true,
                         minifyJS: true,
-                        collapseWhitespace: true
-                    }
-                })
+                        collapseWhitespace: true,
+                    },
+                }),
             );
         });
     return {
         entry,
-        htmlwebpackplugins
+        htmlwebpackplugins,
     };
 };
 const { entry, htmlwebpackplugins } = setMPA();
 
 module.exports = {
-    entry: entry,
+    entry,
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: '[name]_[chunkhash:4].js'
+        filename: '[name]_[chunkhash:4].js',
     },
     stats: 'errors-only',
     mode: 'production',
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
-            "react": path.join(__dirname, "node_modules/react/cjs/react.production.min.js"),
-            "react-dom": path.join(__dirname, "node_modules/react-dom/cjs/react-dom.production.min.js"),
-            "lodash": path.join(__dirname, "node_modules/lodash/lodash.min.js"),
+            '@': path.resolve(__dirname, './src'),
+            react: path.join(__dirname, 'node_modules/react/cjs/react.production.min.js'),
+            'react-dom': path.join(__dirname, 'node_modules/react-dom/cjs/react-dom.production.min.js'),
+            lodash: path.join(__dirname, 'node_modules/lodash/lodash.min.js'),
         },
         modules: [path.join(__dirname, 'node_modules')],
-        extensions: ['.ts', '.tsx', '.json'] //指定解析的后缀,如果不指定webpack默认直解析js|json
+        extensions: ['.ts', '.tsx', '.json'], // 指定解析的后缀,如果不指定webpack默认直解析js|json
     },
     module: {
         rules: [
@@ -72,7 +72,7 @@ module.exports = {
                 test: /.tsx?$/,
                 exclude: /node_modules/,
                 use: [
-                    'ts-loader'
+                    'ts-loader',
                 ],
             },
             {
@@ -83,29 +83,29 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             modules: true,
-                        }
+                        },
                     },
                     {
-                        loader: 'postcss-loader', //使用postcss进行css样式后缀的补全
+                        loader: 'postcss-loader', // 使用postcss进行css样式后缀的补全
                         options: {
                             plugins: () => [
                                 require('autoprefixer')({
-                                    overrideBrowserslist: ['last 2 version', '>1%', 'ios 7']
-                                })
-                            ]
-                        }
+                                    overrideBrowserslist: ['last 2 version', '>1%', 'ios 7'],
+                                }),
+                            ],
+                        },
                     },
                     'less-loader',
-                ]
+                ],
             },
             {
                 test: /.(png|jepg|jpg|gif)$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
-                        name: '[name]_[hash:8].[ext]'
-                    }
-                }]
+                        name: '[name]_[hash:8].[ext]',
+                    },
+                }],
             },
             {
                 test: /.(png|jepg|jpg|gif)$/,
@@ -113,9 +113,9 @@ module.exports = {
                     loader: 'url-loader',
                     options: {
                         name: '[name]_[hash:8].[ext]',
-                        limit: 10240 // 小于10M文件使用url-loader
-                    }
-                }]
+                        limit: 10240, // 小于10M文件使用url-loader
+                    },
+                }],
             },
             {
                 test: /.(woff|woff2|eot|ttf|otf)$/,
@@ -123,17 +123,17 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            name: '[name]_[hash:8].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
+                            name: '[name]_[hash:8].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
     },
     plugins: [
         new CleanWebpackPlugin(),
         new Webpack.DllReferencePlugin({
-            manifest: require('./build/library/library.json')
+            manifest: require('./build/library/library.json'),
         }),
         // new BundleAnalyzerPlugin(),
         new Frienderrorsonly(),
@@ -147,13 +147,13 @@ module.exports = {
             });
         },
         new MiniCssExtractPlugin({
-            filename: '[name]_[contenthash:4].css'
+            filename: '[name]_[contenthash:4].css',
         }),
         new OptimizeCSSAssetsPlugin({
             assetNameRegExp: /\.css$/g,
-            cssProcessor: require('cssnano') //css处理器
+            cssProcessor: require('cssnano'), // css处理器
         }),
-        new HardSourceWebpackPlugin()
+        new HardSourceWebpackPlugin(),
     ].concat(htmlwebpackplugins),
     optimization: {
         splitChunks: {
@@ -163,15 +163,15 @@ module.exports = {
                     test: /(react|react-dom)/,
                     name: 'commons',
                     chunks: 'all',
-                    minChunks: 2
-                }
-            }
+                    minChunks: 2,
+                },
+            },
         },
         minimizer: [
-            new TerserPlugin({ //开启并行压缩
+            new TerserPlugin({ // 开启并行压缩
                 parallel: true,
-                cache: true//开启缓存
-            })
-        ]
-    }
+                cache: true, // 开启缓存
+            }),
+        ],
+    },
 };
